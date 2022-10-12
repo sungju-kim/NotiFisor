33f0a8aab25245e6d8132ffaff2f0b4d48ae3bbb
sungju-kim <wnwn0719@gmail.com>
2022-10-12 15:26:37 +0900

    add: 프로젝트 생성

---
+++ Notifisor/Notifisor/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Notifisor
+//
+//  Created by dale on 2022/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Notifisor/Notifisor/NotifisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifisorApp.swift
+//  Notifisor
+//
+//  Created by dale on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct NotifisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
