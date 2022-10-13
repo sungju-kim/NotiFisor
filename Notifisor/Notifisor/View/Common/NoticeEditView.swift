3c480c046d3974556e8105607d4bc5a684e1b3c6
sungju-kim <wnwn0719@gmail.com>
2022-10-13 11:58:46 +0900

    add: NoticeEditView 생성

---
+++ Notifisor/Notifisor/View/Common/NoticeEditView.swift
@@ -0,0 +1,19 @@
+//
+//  NoticeEditView.swift
+//  Notifisor
+//
+//  Created by dale on 2022/10/13.
+//
+
+import SwiftUI
+
+struct NoticeEditView: View {
+    var body: some View {
+    }
+}
+
+struct NoticeEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoticeEditView()
+    }
+}
