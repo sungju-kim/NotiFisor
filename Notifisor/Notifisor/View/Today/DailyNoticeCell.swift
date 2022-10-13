4ae38ad00b706803a42735d666876362eee055a7
Damagucci-Juice <jsr9874@gmail.com>
2022-10-13 11:48:55 +0900

    feat: DailyNoticeCell 구현 #1

---
+++ Notifisor/Notifisor/View/Today/DailyNoticeCell.swift
@@ -0,0 +1,36 @@
+//
+//  DailyNoticeCell.swift
+//  Notifisor
+//
+//  Created by dale on 2022/10/13.
+//
+
+import SwiftUI
+
+struct DailyNoticeCell: View {
+    var body: some View {
+        VStack(alignment: .trailing, spacing: 40) {
+            HStack {
+                Text("💊 약 먹기")
+                    .font(.title)
+                Spacer()
+                Text("30분 일정")
+            }
+            
+            HStack {
+                Text("21:00 알림 예정")
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .padding()
+        .background(.ultraThickMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .shadow(radius: 1, x: 5, y: 5)
+    }
+}
+
+struct DailyNoticeCell_Previews: PreviewProvider {
+    static var previews: some View {
+        DailyNoticeCell()
+    }
+}
