7c7021d679944b15bf5ff8fc9c2fad2f46cde42f
sungju-kim <wnwn0719@gmail.com>
2022-10-13 15:34:08 +0900

    feat: Unit 구현

--- Notifisor/Notifisor/Model/Unit.swift
+++ Notifisor/Notifisor/Model/Unit.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+enum Unit: String, CaseIterable {
+    case hour
+    case minute
+    case km
+    case times
+
+    var text: String {
+        switch self {
+        case .hour:
+            return "시간"
+        case .minute:
+            return "분"
+        case .km:
+            return "km"
+        case .times:
+            return "회"
+        }
+    }
+
+    var per: [String] {
+        switch self {
+        case .hour, .km:
+            return ["1", "2", "3", "4", "5"]
+        case .minute, .times:
+            return ["15", "30", "45"]
+        }
+    }
+}
