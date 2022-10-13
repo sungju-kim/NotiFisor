fc6c2a280e64dbc02ebfac0e9352061c8afd7cff
sungju-kim <wnwn0719@gmail.com>
2022-10-13 17:21:50 +0900

    add: HistoryNoticeCell 생성
    #8

---
+++ Notifisor/Notifisor/View/DailyHistory/HistoryNoticeCell.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryNoticeCell.swift
+//  Notifisor
+//
+//  Created by dale on 2022/10/13.
+//
+
+import SwiftUI
+
+struct HistoryNoticeCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistoryNoticeCell_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryNoticeCell()
+    }
+}
