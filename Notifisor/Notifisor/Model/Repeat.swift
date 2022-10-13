6b050fe5f0cf077993253a347e25ff61d4457d0b
Damagucci-Juice <jsr9874@gmail.com>
2022-10-13 16:41:14 +0900

    refactor: String RawValue 값 변경
    #3

--- Notifisor/Notifisor/Model/Repeat.swift
+++ Notifisor/Notifisor/Model/Repeat.swift
@@ -8,14 +8,11 @@
 import Foundation
 
 enum Repeat: String, CaseIterable {
-    case everyDay
-    case everyWeek
-    case everyMonth
-    case everyMonday
-    case everyTuesday
-    case everyWednesday
-    case everyThursday
-    case everyFriday
-    case everySaturday
-    case everySunday
+    case everyMonday = "월요일마다"
+    case everyTuesday = "화요일마다"
+    case everyWednesday = "수요일마다"
+    case everyThursday = "목요일마다"
+    case everyFriday = "금요일마다"
+    case everySaturday = "토요일마다"
+    case everySunday = "일요일마다"
 }
