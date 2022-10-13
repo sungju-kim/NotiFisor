5ad2e25f8a5ea4d437f609d8a6c7ad3563c24556
Damagucci-Juice <jsr9874@gmail.com>
2022-10-13 21:02:03 +0900

    add: FulfillmentView 추가
    #5

---
+++ Notifisor/Notifisor/View/History/FulfillmentView.swift
@@ -0,0 +1,20 @@
+//
+//  FulfillmentView.swift
+//  Notifisor
+//
+//  Created by YEONGJIN JANG on 2022/10/13.
+//
+
+import SwiftUI
+
+struct FulfillmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FulfillmentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FulfillmentView()
+    }
+}
