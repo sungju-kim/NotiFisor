49be9533aa536094c3e0e2c9eef921a2452c6c99
Damagucci-Juice <jsr9874@gmail.com>
2022-10-14 12:32:58 +0900

    feat: 스와이프 기능 추가
    - drag 추가
    Closes #9

--- Notifisor/Notifisor/Main/ContentView.swift
+++ Notifisor/Notifisor/Main/ContentView.swift
@@ -12,16 +12,28 @@ struct ContentView: View {
     @State var showMenu = false
 
     var body: some View {
-        GeometryReader { geometry in
-            DailyNoticeView(showMenu: $showMenu)
-                .frame(width: geometry.size.width, height: geometry.size.height)
-                .offset(x: self.showMenu ? geometry.size.width/2 : 0)
-//                .disabled(self.showMenu ? true : false)
-            if self.showMenu {
-                MenuView()
-                    .frame(width: geometry.size.width/2)
-                    .transition(.move(edge: .leading))
+        let drag = DragGesture()
+            .onEnded {
+                if $0.translation.width < -100 {
+                    withAnimation {
+                        self.showMenu = false
+                    }
+                }
             }
+
+        return GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                DailyNoticeView(showMenu: $showMenu)
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+                    .disabled(self.showMenu ? true : false)
+                if self.showMenu {
+                    MenuView()
+                        .frame(width: geometry.size.width/2)
+                        .transition(.move(edge: .leading))
+                }
+            }
+            .gesture(drag)
         }
     }
 }
