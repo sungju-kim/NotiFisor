6da457f049dd0643daa74293727183bad7b9e279
Damagucci-Juice <jsr9874@gmail.com>
2022-10-14 12:14:45 +0900

    feat: show menu에 따라 UI이 얼마만큼 배분할지 설정
    - .disabled 를 포함할지 말지 결정해야함
    
    #9

--- Notifisor/Notifisor/Main/ContentView.swift
+++ Notifisor/Notifisor/Main/ContentView.swift
@@ -14,11 +14,13 @@ struct ContentView: View {
     var body: some View {
         GeometryReader { geometry in
             DailyNoticeView(showMenu: $showMenu)
-                .frame(width: geometry.size.width,
-                       height: geometry.size.height)
+                .frame(width: geometry.size.width, height: geometry.size.height)
+                .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+//                .disabled(self.showMenu ? true : false)
             if self.showMenu {
                 MenuView()
                     .frame(width: geometry.size.width/2)
+                    .transition(.move(edge: .leading))
             }
         }
     }
