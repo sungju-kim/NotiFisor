7b4d3aa9698063bb76fe925d4bf327e2cb0dd5f4
sungju-kim <wnwn0719@gmail.com>
2022-10-17 11:25:15 +0900

    feat: CalendarView구현
    #6

--- Notifisor/Notifisor/View/History/CalendarView.swift
+++ Notifisor/Notifisor/View/History/CalendarView.swift
@@ -4,22 +4,74 @@
 //
 //  Created by dale on 2022/10/13.
 //
-
 import SwiftUI
 
 struct CalendarView: View {
-       var body: some View {
-           LazyVGrid(columns: Array(repeating: GridItem(), count: 7), alignment: .center, spacing: 16) {
-               ForEach(1..<32) { index in
-                   DayCell(day: index)
-               }
-           }
-           .padding()
-           .background(.ultraThickMaterial)
-           .clipShape(RoundedRectangle(cornerRadius: 10))
-           .shadow(radius: 1, x: 5, y: 5)
+    @Environment(\.calendar) var calendar
 
-       }
+    @State var month: Date = .now
+
+    private var beforeMonth: Date {
+        return Calendar.current.date(byAdding: .month, value: -1, to: month) ?? Date()
+    }
+
+    private var nextMonth: Date {
+        return Calendar.current.date(byAdding: .month, value: 1, to: month) ?? Date()
+    }
+
+    var body: some View {
+        NavigationView {
+            LazyVGrid(columns: Array(repeating: GridItem(), count: 7), alignment: .center, spacing: 16) {
+                Section {
+                    ForEach(days(for: month), id: \.self) { date in
+                        let cell = DayCell(day: date.get(.day))
+                        if calendar.isDate(date, equalTo: month, toGranularity: .month) {
+                            cell
+                        } else {
+                            cell.hidden()
+                        }
+                    }
+                } header: {
+                    HStack {
+                        Button {
+                            month = beforeMonth
+                        } label: {
+                            Image(systemName: "chevron.left")
+                        }
+
+                        Text(DateFormatter.monthAndYear.string(from: month))
+                            .font(.title)
+
+                        Button {
+                            month = nextMonth
+                        } label: {
+                            Image(systemName: "chevron.right")
+                        }
+
+                    }
+                }
+
+            }
+            .padding()
+            .background(.ultraThickMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .shadow(radius: 1, x: 5, y: 5)
+        }
+
+    }
+
+    private func days(for month: Date) -> [Date] {
+        guard
+            let monthInterval = calendar.dateInterval(of: .month, for: month),
+            let monthFirstWeek = calendar.dateInterval(of: .weekOfMonth, for: monthInterval.start),
+            let monthLastWeek = calendar.dateInterval(of: .weekOfMonth, for: monthInterval.end)
+        else { return [] }
+
+        return calendar.generateDates(
+            inside: DateInterval(start: monthFirstWeek.start, end: monthLastWeek.end),
+            matching: DateComponents(hour: 0, minute: 0, second: 0)
+        )
+    }
 }
 
 struct CalendarView_Previews: PreviewProvider {
