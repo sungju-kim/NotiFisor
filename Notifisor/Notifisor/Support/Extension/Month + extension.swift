e8b4d849dd630a34d969b98e7e63f1475efe04fa
Damagucci-Juice <jsr9874@gmail.com>
2022-10-19 19:09:32 +0900

    feat: Data Model Class 인 Month 에 extension 추가
    #27

--- Notifisor/Notifisor/Support/Extension/Month + extension.swift
+++ Notifisor/Notifisor/Support/Extension/Month + extension.swift
@@ -4,5 +4,33 @@
 //
 //  Created by YEONGJIN JANG on 2022/10/19.
 //
-
+import CoreData
 import Foundation
+
+extension Month {
+    //MARK: - making entity, saving context
+    static func add12MonthEntity() {
+        let context = PersistenceController.shared.container.viewContext
+
+        for monthNumber in 1...12 {
+            let month = Month(context: context)
+            let representDate = Calendar.generateMonthDate(2022, monthNumber)
+            let datesOfMonth = Calendar.days(for: representDate)
+
+            let days: [Day] = datesOfMonth.compactMap { dateElement in
+                let day = Day(context: context)
+                day.date = dateElement
+                return day
+            }
+
+            month.date = representDate
+            month.days = NSSet(array: days)
+        }
+
+        do {
+            try context.save()
+        } catch {
+            print("error: fail at saving 12 month")
+        }
+    }
+}
