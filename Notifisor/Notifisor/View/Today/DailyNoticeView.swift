430469338de7a253b4cc8c1083ae86c70213c0e9
Damagucci-Juice <jsr9874@gmail.com>
2022-10-20 13:48:05 +0900

    feat: DailyNotice View 에서 엔티티 추가하는 버튼 생성
    #27

--- Notifisor/Notifisor/View/Today/DailyNoticeView.swift
+++ Notifisor/Notifisor/View/Today/DailyNoticeView.swift
@@ -45,6 +45,13 @@ struct DailyNoticeView: View {
                         .sheet(isPresented: $showSheet) {
                             NoticeEditView()
                         }
+
+                        Button {
+                            Month.add12MonthEntity()
+                        } label: {
+                            ColoredImage(source: "12.square")
+                            Text("add monthes")
+                        }
                     }
 
                     ToolbarItemGroup(placement: .navigationBarLeading) {
