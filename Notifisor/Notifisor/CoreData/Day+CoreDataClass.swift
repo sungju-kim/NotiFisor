3b1c11c805bc3b8faf933352787047d076684c12
Damagucci-Juice <jsr9874@gmail.com>
2022-10-20 14:42:03 +0900

    feat: Core Data 에서 Day 타입을 매뉴얼하게 추가함
    #27

---
+++ Notifisor/Notifisor/CoreData/Day+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Day+CoreDataClass.swift
+//  Notifisor
+//
+//  Created by YEONGJIN JANG on 2022/10/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Day)
+public class Day: NSManagedObject {
+
+}
