c5c81142ab7f78eef1d856d68e4b2a87d6bf9be3
sungju-kim <wnwn0719@gmail.com>
2022-11-16 19:50:43 +0900

    Refactor: DailyNoticeView에 Day 주입

--- Notifisor/Notifisor/Main/ContentView.swift
+++ Notifisor/Notifisor/Main/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by dale on 2022/10/12.
 //
 
+import RealmSwift
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +13,12 @@ struct ContentView: View {
     @State var showMenu = false
     @State var showHistory = false
     @State var showProfile = false
+
+    @EnvironmentObject var repository: NoticeRepository
+
+    private var day: Day {
+        return repository.get(Day.self, Date.now.id) as? Day ?? Day()
+    }
 
     var body: some View {
         let drag = DragGesture()
@@ -25,7 +32,8 @@ struct ContentView: View {
             ZStack(alignment: .leading) {
                 DailyNoticeView(showMenu: $showMenu,
                                 showHistory: $showHistory,
-                                showProfile: $showProfile
+                                showProfile: $showProfile,
+                                day: day
                 )
                 .frame(width: geometry.size.width, height: geometry.size.height)
                 .offset(x: showMenu ? geometry.size.width/2 : 0)
