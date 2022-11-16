582a4a81bec7a208dd9d436a2dd1454938436946
sungju-kim <wnwn0719@gmail.com>
2022-11-16 19:51:11 +0900

    Refactor: NoticeEditView 가독성 개선

--- Notifisor/Notifisor/Model/RepeatDay.swift
+++ Notifisor/Notifisor/Model/RepeatDay.swift
@@ -9,19 +9,21 @@ import Foundation
 
 struct RepeatDay {
     enum WeekDay: String, CaseIterable {
+        case everySunday = "일요일마다"
         case everyMonday = "월요일마다"
         case everyTuesday = "화요일마다"
         case everyWednesday = "수요일마다"
         case everyThursday = "목요일마다"
         case everyFriday = "금요일마다"
         case everySaturday = "토요일마다"
-        case everySunday = "일요일마다"
     }
 
     let weekDay: WeekDay
     var isSelected: Bool = false
     var toInt: Int {
         switch self.weekDay {
+        case .everySunday:
+            return 1
         case .everyMonday:
             return 2
         case .everyTuesday:
@@ -34,8 +36,6 @@ struct RepeatDay {
             return 6
         case .everySaturday:
             return 7
-        case .everySunday:
-            return 1
         }
     }
 }
