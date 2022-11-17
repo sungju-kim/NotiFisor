9f8dbfaae49a901fd31b4c38f80c1a754b5da71e
Damagucci-Juice <jsr9874@gmail.com>
2022-11-17 15:34:21 +0900

    refactor: Menu 구현부 옮김
    #37

--- Notifisor/Notifisor/View/Today/EllipsisView.swift
+++ Notifisor/Notifisor/View/Today/EllipsisView.swift
@@ -4,11 +4,33 @@
 //
 //  Created by YEONGJIN JANG on 2022/11/17.
 //
-
+import RealmSwift
 import SwiftUI
 
 struct EllipsisView: View {
+    @EnvironmentObject var noticeRepository: NoticeRepository
+    @ObservedRealmObject var notice: Notice
+    @Binding var isShowEditSheet: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Menu {
+            Button("완료", action: {
+                do {
+                    try noticeRepository.realm.write {
+                        notice.thaw()?.isDone.toggle()
+                    }
+                } catch(let error) {
+                    print(error.localizedDescription)
+                }
+            })
+            Button("수정", action: {
+                isShowEditSheet.toggle()
+            })
+        } label: {
+            Image(systemName: "ellipsis")
+                .rotationEffect(.degrees(90), anchor: .top)
+                .frame(width: 40, height: 40)
+                .foregroundColor(.secondary)
+        }
     }
 }
