b93bdf04497fdbc1757b2889a97be8c4950a9607
Damagucci-Juice <jsr9874@gmail.com>
2022-11-17 15:29:23 +0900

    add: Menu 를 분리할 EllipsisView 추가
    #37

---
+++ Notifisor/Notifisor/View/Today/EllipsisView.swift
@@ -0,0 +1,14 @@
+//
+//  EllipsisView.swift
+//  Notifisor
+//
+//  Created by YEONGJIN JANG on 2022/11/17.
+//
+
+import SwiftUI
+
+struct EllipsisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
