9118f8a0375d751b2419e6bf51cc58a4135bd2be
Damagucci-Juice <jsr9874@gmail.com>
2022-11-17 17:24:16 +0900

    add: 완료 표시를 커스텀하게 나타내줄 뷰 추가
    #37

---
+++ Notifisor/Notifisor/View/Today/CheckerMarkView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckerMarkView.swift
+//  Notifisor
+//
+//  Created by YEONGJIN JANG on 2022/11/17.
+//
+
+import SwiftUI
+
+struct CheckerMarkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckerMarkView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckerMarkView()
+    }
+}
