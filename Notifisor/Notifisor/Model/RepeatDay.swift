9cd0c92152a6d8c379c37fc7de39b538e1855bde
sungju-kim <wnwn0719@gmail.com>
2022-11-17 19:29:56 +0900

    refactor: RepeatDay, Week 분리

--- Notifisor/Notifisor/Model/RepeatDay.swift
+++ Notifisor/Notifisor/Model/RepeatDay.swift
@@ -8,34 +8,35 @@
 import Foundation
 
 struct RepeatDay {
-    enum WeekDay: String, CaseIterable {
-        case everySunday = "일요일마다"
-        case everyMonday = "월요일마다"
-        case everyTuesday = "화요일마다"
-        case everyWednesday = "수요일마다"
-        case everyThursday = "목요일마다"
-        case everyFriday = "금요일마다"
-        case everySaturday = "토요일마다"
-    }
-
-    let weekDay: WeekDay
+    let week: Week
     var isSelected: Bool = false
-    var toInt: Int {
-        switch self.weekDay {
-        case .everySunday:
-            return 1
-        case .everyMonday:
-            return 2
-        case .everyTuesday:
-            return 3
-        case .everyWednesday:
-            return 4
-        case .everyThursday:
-            return 5
-        case .everyFriday:
-            return 6
-        case .everySaturday:
-            return 7
+}
+
+enum Week: Int, CaseIterable {
+    case sun = 1
+    case mon
+    case tues
+    case wednes
+    case thurs
+    case fri
+    case satur
+
+    var text: String {
+        switch self {
+        case .sun:
+            return "일요일"
+        case .mon:
+            return "월요일"
+        case .tues:
+            return "화요일"
+        case .wednes:
+            return "수요일"
+        case .thurs:
+            return "목요일"
+        case .fri:
+            return "금요일"
+        case .satur:
+            return "토요일"
         }
     }
 }
