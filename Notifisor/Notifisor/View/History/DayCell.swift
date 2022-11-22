0e14b4fd1bf61b1b804bc003119eee7713021458
sungju-kim <wnwn0719@gmail.com>
2022-11-22 16:54:33 +0900

    refactor: DayCell layout 수정 #40

--- Notifisor/Notifisor/View/History/DayCell.swift
+++ Notifisor/Notifisor/View/History/DayCell.swift
@@ -31,17 +31,18 @@ struct DayCell: View {
                     .foregroundColor(.primary)
                 
                 Circle()
-                    .stroke(Color.gray, lineWidth: 5)
+                    .stroke(Color.gray, lineWidth: 3)
                     .opacity(0.1)
 
                 Circle()
                     .trim(from: 0, to: percentage)
-                    .stroke(style: StrokeStyle(lineWidth: 5,
+                    .stroke(style: StrokeStyle(lineWidth: 3,
                                                lineCap: .round,
                                                lineJoin: .round))
                     .foregroundColor(.yellow)
                     .rotationEffect(.degrees(-90))
             }
+            .frame(width: 35, height: 35)
         }
     }
 }
