04264878e717586a4e5ebe4d32a91c4cf05d4723
sungju-kim <wnwn0719@gmail.com>
2022-11-29 14:09:10 +0900

    refactor: 완료 알림 표시 방법 변경

--- Notifisor/Notifisor/View/Today/DailyNoticeCell.swift
+++ Notifisor/Notifisor/View/Today/DailyNoticeCell.swift
@@ -10,39 +10,50 @@ import SwiftUI
 
 struct DailyNoticeCell: View {
     @ObservedRealmObject var notice: CurrentNotice
+    @EnvironmentObject var noticeRepository: NoticeRepository
+
     @State private var isShowEditSheet = false
 
     var body: some View {
-        HStack(alignment: .top) {
-            VStack(alignment: .leading) {
-                HStack(alignment: .top) {
-                    Text(notice.title)
-                        .font(.title2)
-                        .lineLimit(2)
-                        .layoutPriority(1)
+        VStack(alignment: .leading) {
+            HStack {
+                Text(notice.title)
+                    .font(.title2)
+                    .lineLimit(2)
+                    .layoutPriority(1)
 
-                    Spacer()
+                Spacer()
 
-                    EllipsisView(notice: notice, isShowEditSheet: $isShowEditSheet)
-                        .frame(width: 30, height: 30)
+                Button {
+                    noticeRepository.write {
+                        notice.thaw()?.isDone.toggle()
+                    }
+                } label: {
+                    if notice.isDone {
+                        Circle()
+                            .fill(Constant.pointColor)
+                    } else {
+                        Circle()
+                            .strokeBorder(Color(.gray), lineWidth: 3)
+                    }
                 }
-
-                HStack {
-                    Text(notice.amount ?? 0, format: .number)
-                    Text(notice.unit.text)
-
-                    Spacer()
-
-                    Text(notice.noticeTime, format: .dateTime.hour().minute())
-                    Text("알림 예정")
-                }
-                .foregroundStyle(.secondary)
-                .padding(.top, 4)
+                .frame(width: 20, height: 20)
             }
+
+            HStack {
+                Text(notice.amount ?? 0, format: .number)
+                Text(notice.unit.text)
+
+                Spacer()
+
+                Text(notice.noticeTime, format: .dateTime.hour().minute())
+                Text("알림 예정")
+            }
+            .foregroundStyle(.secondary)
+            .padding(.top, 4)
         }
         .padding()
         .background(.ultraThickMaterial)
-        .background(notice.isDone ? Constant.pointColor : Constant.background)
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .shadow(radius: 1, x: 5, y: 5)
         .sheet(isPresented: $isShowEditSheet) {
